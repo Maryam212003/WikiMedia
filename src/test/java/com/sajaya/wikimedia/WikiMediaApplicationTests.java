b87daa96aa88b576517a00b701e82dddf203fd21
Maryam sadat <maryam.s01382@gmail.com>
2024-08-06 16:34:29 +0330

    project setup

---
+++ src/test/java/com/sajaya/wikimedia/WikiMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajaya.wikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
