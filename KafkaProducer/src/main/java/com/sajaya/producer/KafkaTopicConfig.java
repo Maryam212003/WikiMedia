b51bb9064ab3593f191a0da8c86476b525e0a9ea
Maryam.s <maryam.s01382@gmail.com>
2024-08-09 11:35:33 +0330

    add properties and topic config

---
+++ KafkaProducer/src/main/java/com/sajaya/producer/KafkaTopicConfig.java
@@ -0,0 +1,15 @@
+package com.sajaya.producer;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    public NewTopic topic() {
+        return TopicBuilder.name("WikiMediaRecentChange")
+                //.partition
+                .build();
+    }
+}
