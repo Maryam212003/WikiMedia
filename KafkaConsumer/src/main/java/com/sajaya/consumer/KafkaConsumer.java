9886dab6cafad71631dc5348af9c6980a1486eb3
Maryam sadat <maryam.s01382@gmail.com>
2024-08-12 12:37:29 +0330

    add consumer

---
+++ KafkaConsumer/src/main/java/com/sajaya/consumer/KafkaConsumer.java
@@ -0,0 +1,29 @@
+package com.sajaya.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+        private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+        @KafkaListener(
+                topics = "WikiMediaRecentChange",
+                groupId = "myGroup"
+        )
+        public void consume(String eventMessage){
+
+            LOGGER.info(String.format("consumer 1: Event message received -> %s", eventMessage));
+        }
+
+        @KafkaListener(
+                topics = "WikiMediaRecentChange",
+                groupId = "myGroup"
+        )
+        public void consume2(String eventMessage){
+
+                LOGGER.info(String.format("consumer 2: Event message received -> %s", eventMessage));
+        }
+}
